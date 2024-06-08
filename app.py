663a7d03394bd4a30fdc5d62154dfa178449f6cc
al2h36o30s3 <xieqy28@mideazy.com>
2024-06-08 23:44:29 +0800

    add app.py requirements.txt packages.txt

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
 
 # download internlm2 to the base_path directory using git tool
-base_path = './personal_assistant'
+base_path = './assistant'
 os.system(f'git clone https://code.openxlab.org.cn/Al2H36O30S3/assistant.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
 
